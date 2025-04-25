374f7ae7579d4de0e3eacdb8d051352d16f635c6
Ajay. M <ajayyym2025@gmail.com>
2025-04-25 23:50:02 +0530

    Completed

--- firebase.js
+++ firebase.js
@@ -2,15 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-// TODO: Replace with your Firebase project config
+// Replace with your Firebase project config!
 const firebaseConfig = {
-    apiKey: "AIzaSyDoC8sRRBbDsfuVcOOo3cO2-VPhwD_5A3A",
-    authDomain: "genai-4028a.firebaseapp.com",
-    projectId: "genai-4028a",
-    storageBucket: "genai-4028a.firebasestorage.app",
-    messagingSenderId: "381601954348",
-    appId: "1:381601954348:web:53b192d67e0f79989eaf89",
-    measurementId: "G-R8165G1WF2"
+    apiKey: "Enter_Api_key",
+    authDomain: "Enter_auth_domain_link",
+    projectId: "Enter_project_id",
+    storageBucket: "Enter_Storage_Bucket",
+    messagingSenderId: "Enter_message_id",
+    appId: "Enter_app_id",
+    measurementId: "Enter_measurement_id"
   };
 
 // Initialize Firebase
